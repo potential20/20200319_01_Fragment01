c3f06dc699f725f71f385d4e6b8cef2c0783c44b
potential20 <passion212@nate.com>
2020-03-19 11:45:29 +0900

    아이디 입력칸 그리기 + colors.xml 추가(!)

--- app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragment/SecondFragment.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragment/SecondFragment.java
@@ -4,23 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import kr.co.tjoeun.a20200319_01_fragment01.R;
+import kr.co.tjoeun.a20200319_01_fragment01.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
+
+    FragmentSecondBinding binding = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second,container,false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second,container,false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        String id = binding.idEdt.getText().toString();
+        String pw = binding.pwEdt.getText().toString();
+
+        Toast.makeText(getActivity(),"관리자 로그인", Toast.LENGTH_SHORT).show();
+
+
     }
 }
