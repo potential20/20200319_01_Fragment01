5df003935713ad6a8e188a78da35aaadc2b79e7f
potential20 <passion212@nate.com>
2020-03-19 10:14:06 +0900

    두번째 프래그먼트 자바 작성  - onCreateView  - onActivityCreated

--- app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragment/SecondFragment.java
+++ app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/fragment/SecondFragment.java
@@ -1,6 +1,26 @@
 package kr.co.tjoeun.a20200319_01_fragment01.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import kr.co.tjoeun.a20200319_01_fragment01.R;
+
 public class SecondFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_second,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
