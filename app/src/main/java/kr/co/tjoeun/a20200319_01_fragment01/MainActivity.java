530390434fe3c0e68ad88826eb1361cb3bee941a
potential20 <passion212@nate.com>
2020-03-19 09:41:25 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200319_01_fragment01/MainActivity.java
@@ -0,0 +1,32 @@
+package kr.co.tjoeun.a20200319_01_fragment01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200319_01_fragment01.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setupEvents();
+        setupValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setupValues() {
+
+    }
+}
